9fe3e7f7b4080f568762a49a275bf381949c6391
Sumit Suman <sumitsuman6258@gmail.com>
2023-12-28 10:13:35 +0530

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,3 +4,10 @@ Game::Game() : window(sf::VideoMode(800, 600), "SFML Game"), player(sf::Vector2f
 
 void Game::run() {
     sf::Clock clock;
+    while (window.isOpen()) {
+        deltaTime = clock.restart();
+        processEvents();
+        update(deltaTime);
+        render();
+    }
+}
