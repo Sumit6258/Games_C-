a5b23c1e90c137e3a59ecec107fe7cf53f46c2e1
Sumit Suman <sumitsuman6258@gmail.com>
2023-12-28 10:11:22 +0530

    Update Game.h

--- Game.h
+++ Game.h
@@ -10,3 +10,10 @@ private:
     void processEvents();
     void update(sf::Time deltaTime);
     void render();
+
+private:
+    sf::RenderWindow window;
+    sf::RectangleShape player;
+    float velocity;
+    sf::Time deltaTime;
+};
